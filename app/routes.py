d56182090e88598438415ff8552eaeb479500115
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2023-03-21 22:34:27 -0400

    Add form

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,21 @@
 from app import app
 from flask import render_template, redirect, url_for, flash
-
-
+from app.forms import SignUpForm
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/submission')
+@app.route('/submission', methods = ['GET', 'POST'])
 def submission():
-    return render_template('submission.html')
+    form = SignUpForm()
+    if form.validate_on_submit():
+        print("Validated.")
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        phone_number = form.phone_number.data
+        address = form.address.data
+        print(first_name, last_name, phone_number, address)
+        return redirect(url_for('index'))
+    return render_template('submission.html', form=form)
