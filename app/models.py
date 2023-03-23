1519593907bc08bbbd2c8e35236615927e68ab18
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2023-03-22 22:58:53 -0400

    Create database with migrate

--- app/models.py
+++ app/models.py
@@ -6,8 +6,8 @@ class Entry(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
-    phone_number = db.Column(db.String(75), nullable=False, unique=True),
-    address = db.Column(db.String(100), nullable=False),
+    phone_number = db.Column(db.String(75), nullable=False, unique=True)
+    address = db.Column(db.String(100), nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
 
