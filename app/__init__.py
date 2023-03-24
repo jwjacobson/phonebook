e7f45ba224774682226e96fe638ae77d28b9504d
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2023-03-24 19:07:49 -0400

    Update init.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,13 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
+
+login = LoginManager(app)
+# Tell the login manager where to redirect if a user is not logged in
+login.login_view = 'login'
+# login.login_message = "Hey you can't do that!"
+login.login_message_category = 'danger'
 
 app = Flask(__name__)
 app.config.from_object(Config)
