1b5f631240ec650d450297eccb02ac30c1a747b7
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2023-03-24 19:30:24 -0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
-from app import db
+from app import db, login
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 class Entry(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -8,6 +10,7 @@ class Entry(db.Model):
     phone_number = db.Column(db.String(75), nullable=False)
     address = db.Column(db.String(100), nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) # SQL - FOREIGN KEY(user_id) REFERENCES user(id)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -16,3 +19,28 @@ class Entry(db.Model):
 
     def __repr__(self):
         return f"<Entry {self.id}|{self.first_name} {self.last_name}>"
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(75), nullable=False, unique=True)
+    username = db.Column(db.String(75), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+    entries = db.relationship('Entry', backref='submitter')
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.password = generate_password_hash(kwargs.get('password'))
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f"<User {self.id}|{self.username}>"
+
+    def check_password(self, password_guess):
+        return check_password_hash(self.password, password_guess)
+
+@login.user_loader
+def get_a_user_by_id(user_id):
+    return db.session.get(User, user_id)
